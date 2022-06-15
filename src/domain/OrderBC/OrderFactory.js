8ab81d22372461a0393fd73361225f3d4f967f87
Tran Tu <65528999+GitHubCodeForLife@users.noreply.github.com>
2022-06-15 17:43:09 +0700

    abc

--- src/domain/OrderBC/OrderFactory.js
+++ src/domain/OrderBC/OrderFactory.js
@@ -1,63 +1,25 @@
 import orderStatus from "./OrderStatusValueObj";
-import { Order, OrderItem } from "./OrderDomainModel";
 
-const createItem = ({
-  price,
-  total,
-  quantity,
-  productId,
-  discountId,
-}) => {
-  // Validate data
-
-  const newItem = {
-    info: {},
-    errMessage: "",
-  };
-
-  newItem.info = new OrderItem({
-    price,
-    total,
-    quantity,
-    productId,
-    discountId,
-  });
-
-  return newItem;
-};
-
-const createOrder = ({
-  total,
-  items,
-  userId,
-  shipDate,
-  shipperId,
-  receiverName,
-  receiverPhone,
-  paymentDetailId,
-  receiverAddress,
-}) => {
-  // Validate data
-
+const createOrder = (data) => {
   const newOrder = {
     info: {},
     errMessage: "",
   };
 
-  const itemList = items && items.forEach((item) => createItem(item));
+  // const itemList = items && items.forEach((item) => createItem(item));
 
-  newOrder.info = new Order({
-    total,
-    userId,
-    shipDate,
-    shipperId,
-    receiverName,
-    receiverPhone,
-    items: itemList,
-    paymentDetailId,
-    receiverAddress,
+  newOrder.info = {
+    total: data.total,
+    userId: data.userId,
+    shipDate: data.shipDate,
+    shipperId: data.shipperId,
+    receiverName: data.receiver_name,
+    receiverPhone: data.receiver_phone,
+    // items: itemList,
+    paymentmethod: data.paymentmethod,
+    receiverAddress: data.receiver_address,
     status: orderStatus.PROCESSING,
-  });
+  };
 
   return newOrder;
 };
